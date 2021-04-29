775cd511acf6f9b718bfc82905a6b7183e099c9b
Santosh Kumar <sntshkmr60@gmail.com>
2021-04-29 12:17:31 +0000

    Adds delete (based on passed data)

--- dsa/linkedlist/python/linked_list.py
+++ dsa/linkedlist/python/linked_list.py
@@ -48,4 +48,19 @@ class LinkedList:
             print(f"{actualNode.data}")
             actualNode = actualNode.nextNode
 
-        
+    def remove(self, data):
+        if self.head is None:
+            return
+
+        self.size = self.size - 1
+        currentNode = self.head
+        previousNode = None
+
+        while currentNode.data != data:
+            previousNode = currentNode
+            currentNode = currentNode.nextNode
+
+        if previousNode is None:
+            self.head = currentNode.nextNode
+        else:
+            previousNode.nextNode = currentNode.nextNode
