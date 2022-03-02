1e07d8821b13fb0914c11889c3f0fa7c515c74c4
Santosh Kumar <sntshkmr60@gmail.com>
2022-03-02 18:15:24 +0000

    Init and allocation at once

--- dsa/array/Array.java
+++ dsa/array/Array.java
@@ -2,15 +2,12 @@ package dev.santoshk;
 
 public class Array {
     public static void main(String args[]) {
-        int []marks = new int[3];
-        marks[0] = 97;  // phy
-        marks[1] = 98;  // chem
-        marks[2] = 95;  // eng
+        int []marks = {97, 98, 95};
 
 //        System.out.println(marks);  // this won't work; this prints memory address of marks
 
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
+        for (int i=0; i<3; i++) {
+            System.out.println(marks[i]);
+        }
     }
 }
